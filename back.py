65301a07d1f2870769c614b8f9b043581681a369
Eric de Lima <eric.vinlima@gmail.com>
2023-09-12 18:40:39 -0300

    fix: remove routes to backend

--- back.py
+++ back.py
@@ -1,81 +1,49 @@
-from flask import Flask, request, jsonify
+import os
+import re
 
-import openai
-
-# Set your OpenAI API key here
-openai.api_key = ''
-
-def generate_response(prompt):
-    """
-    Generate a response using the OpenAI GPT-3 API.
-
-    Args:
-        prompt (str): The input prompt for the GPT-3 model.
-
-    Returns:
-        str: The generated response from the GPT-3 model.
-    """
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo",  # You can choose an appropriate engine
-        prompt=prompt,
-        max_tokens=50  # You can adjust this based on the desired response length
-    )
-    return response.choices[0].text.strip()
+from flask import (
+    Flask,
+    request,
+    jsonify,
+)
 
 
 app = Flask(__name__)
 
-# Endpoint for ampliar
-@app.route('/ampliar', methods=['POST'])
-def ampliar_endpoint():
+
+def update_openai_key_in_env(new_key):
+    with open(".env", "r") as file:
+        content = file.read()
+
+    new_content = re.sub(
+        r'OPENAI_API_KEY=.*',
+        f'OPENAI_API_KEY={new_key}',
+        content,
+    )
+
+    if "OPENAI_API_KEY=" not in content:
+        new_content += f"\nOPENAI_API_KEY={new_key}\n"
+
+    with open(".env", "w") as file:
+        file.write(new_content)
+
+
+@app.route('/b/setToken', methods=['POST'])
+def set_token():
     data = request.get_json()
-    data = "amplie esse texto: " + data['text']
-    # Implement ampliar logic here
-    ampliado = generate_response(data)
-    response = {'result': ampliado}
+    token = data['token']
+    os.environ["OPENAI_API_KEY"] = token
+    update_openai_key_in_env(token)
+    response = {'token': token}
     return jsonify(response)
 
-# Endpoint for corrigir
-@app.route('/corrigir', methods=['POST'])
-def corrigir_endpoint():
-    data = request.get_json()
-    data = "corrija esse texto: " + data['text']
-    # Implement corrigir logic here
-    corrigido = generate_response(data)
-    response = {'result': corrigido}
+
+@app.route('/b/getToken', methods=['GET'])
+def get_token():
+    api_key = os.environ.get("OPENAI_API_KEY")
+    response = {'token': api_key}
     return jsonify(response)
 
-# Endpoint for converterRegiao
-@app.route('/converterRegiao', methods=['POST'])
-def converter_regiao_endpoint():
-    data = request.get_json()
-    regiao =  request.get_json()
-    # Implement converterRegiao logic her◘e
-    data = "Converta este texto para o estilo da região " + data['regiao'] + " do Brasil: " + data['text']
-    convertido = generate_response(data)
-    response = {'result': convertido}
-    return jsonify(response)
-
-# Endpoint for detectarRegiao
-@app.route('/detectarRegiao', methods=['POST'])
-def detectar_regiao_endpoint():
-    data = request.get_json()
-    # Implement detectarRegiao logic here
-    data = "Qual região do Brasil esse texto corresponde: " + data['text']
-    detectado = generate_response(data)
-    response = {'result': detectado}
-    return jsonify(response)
-
-# Endpoint for verificarGramatica
-@app.route('/verificarGramatica', methods=['POST'])
-def verificar_gramatica_endpoint():
-    data = request.get_json()
-    # Implement verificarGramatica logic here
-    data = "corrija esse texto gramaticalmente: " + data['text']
-    verificado = generate_response(data)
-    response = {'result': verificado}
-    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
